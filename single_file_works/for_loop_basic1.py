e2466143a705df96e538c4e6390996832a398549
Michael Smith <mjusmith1@gmail.com>
2023-04-30 15:17:32 -0500

    initial commit

---
+++ single_file_works/for_loop_basic1.py
@@ -0,0 +1,39 @@
+for i in range(151):
+    print(i)
+
+for i in range(5, 1001):
+    if i % 5==0:
+        print(i)
+
+for i in range(1, 101):
+    if i % 10==0:
+        print("Coding Dojo")
+    elif i % 5==0:
+        print("Coding")
+    elif print(i):
+        pass
+
+sum=0
+for i in range(500001):
+    if(i % 2 !=0):
+        sum += i
+    print(sum)
+
+# sum=0
+# for i in range(500000+1):
+#     if(i % 2 !=0):
+#         print("{0}".format(i))
+#         sum+=i
+
+# print(sum)
+
+for i in range(2018, 0, -4):
+    print(i)
+
+lowNum=2
+highNum=9
+mult=3
+
+for i in range(lowNum, highNum+1):
+    if(i % mult ==0):
+        print(i)
