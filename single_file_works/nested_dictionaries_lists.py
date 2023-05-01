093908e2b5876f0c7df69b0eec3a7d37bccf4e77
Michael Smith <mjusmith1@gmail.com>
2023-04-30 22:26:16 -0500

    initial commit

---
+++ single_file_works/nested_dictionaries_lists.py
@@ -0,0 +1,91 @@
+# 1
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+# print(x)
+
+students[0]["last_name"]='Bryant'
+# print(students)
+
+sports_directory["soccer"][0]='Andres'
+# print(sports_directory)
+
+z[0]['y']=30
+print(z)
+
+# 2
+def iterateDictionary(some_list):
+    for each_key in some_list:
+        print(each_key)
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+
+# 3
+def iterateDictionary2(key_name, some_list):
+    for each_key in some_list:
+        print(each_key[key_name])
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+# 4
+def printInfo(some_dict):
+    for x in some_dict:
+        print(len(some_dict[x]))
+        print(x)
+        print(some_dict[x])
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+
+
+# output:
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon
