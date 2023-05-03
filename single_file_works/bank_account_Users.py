a906aa3f4d50751c766022156698d1a0afa3964b
Michael Smith <mjusmith1@gmail.com>
2023-05-02 21:18:12 -0500

    updates

---
+++ single_file_works/bank_account_Users.py
@@ -0,0 +1,78 @@
+class BankAccount:
+    
+    Holders =[]
+    def __init__(self, int_rate=float(), balance=0):
+        self.int_rate= int_rate
+        self.balance= balance
+
+        BankAccount.Holders.append(self)
+
+
+    @classmethod
+    def Get_Info(cls):
+        for holder in cls.Holders:
+            holder.display_account_info()
+
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(self.balance)
+        return self
+    
+    def withdraw(self, amount):
+        if self.balance>amount:
+            self.balance -= amount
+            print(self.balance)
+            return self
+        else:
+            print("Insufficient Funds")
+            return self
+        
+    def display_account_info(self):
+        print(f"{self.balance}\n{self.int_rate}")
+        return self
+    
+    def yield_interest(self):
+        if self.balance>0:
+            self.balance *= self.int_rate
+            print(self.balance)
+            return self
+        else:
+            print("Insufficient funds")
+            return False
+        
+
+class User:
+
+    def __init__(self, name, email):
+        self.account = BankAccount(int_rate=0.02, balance=0)
+        self.name = name
+        self.email = email
+    
+    # other methods
+    
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+    
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        return self
+    
+    def display_user_balance(self):
+        print(self.account.balance)
+        return self
+    
+Holder_3= User("Bob", "Bobbymail@koth.com")
+Holder_3.make_deposit(100)
+Holder_3.display_user_balance()
+
+
+Holder_1= BankAccount(1.05,800)
+Holder_2= BankAccount(1.2,150)
+
+Holder_1.deposit(50).deposit(250).deposit(100).withdraw(200).yield_interest()
+
+Holder_2.deposit(50).deposit(4000).deposit(80).withdraw(20).deposit(400).withdraw(10000).yield_interest().display_account_info()
+
+BankAccount.Get_Info()
