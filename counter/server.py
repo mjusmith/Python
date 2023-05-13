03aeb8bd26b2e9d98a931b774ce333136c5b0c20
Michael Smith <mjusmith1@gmail.com>
2023-05-13 09:50:05 -0500

    session fixes

--- counter/server.py
+++ counter/server.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, redirect, session  # Import Flask to allow us to create our app
 
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-
-count = 0
+app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def show_counter():
-    global count
-    count+=1
-    return render_template('counter.html', count=count)
+    if 'count' not in session:
+        session['count']= 0
+    return render_template('counter.html')
+
+@app.route('/increment', methods=['POST'])
+def increment():
+    session['count']+= 1
+    return redirect('/')
+
+
 
 @app.route('/destroy')
 def destruction():
